5f9008a196c0a046f9885f2ffb77d88a1368b72c
albertalegria <albertalegria@gmail.com>
2017-07-20 12:25:15 +0200

    * ComicBooksController.cs: Modified action method to add Comic book   detail view
    * Detail.cshtml: Added Comic book detail view

--- comic-book-gallery/Controllers/ComicBooksController.cs
+++ comic-book-gallery/Controllers/ComicBooksController.cs
@@ -12,20 +12,29 @@ namespace comicbookgallery.Controllers
         }
         */
 
+
         //public ContentResult Detail()
+        //public ActionResult Detail()
+        //{
+        //    if(DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
+        //    {
+        //        //return new RedirectResult("/");
+        //        return Redirect("/");
+        //    }
+
+        //    /*return new ContentResult()
+        //    {
+        //        Content = "Hello from the comic books controller!"
+        //    };*/
+        //    return Content("Hello from the comic books controller!");
+        //}
+
+
         public ActionResult Detail()
         {
-            if(DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
-            {
-                //return new RedirectResult("/");
-                return Redirect("/");
-            }
-
-            /*return new ContentResult()
-            {
-                Content = "Hello from the comic books controller!"
-            };*/
-            return Content("Hello from the comic books controller!");
+            return View();   
         }
+
+
     }
 }
